6e05f6c2312340e3d964331d60daa887061c894f
parabirb <parabirb@protonmail.ch>
2021-12-21 21:00:51 -0600

    Add bootstrap for electron app

--- client/index.js
+++ client/index.js
@@ -1,3 +1,16 @@
 /*
     antisocial client
 */
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    window.loadFile("src/index.html");
+    window.removeMenu();
+}
+
+app.whenReady().then(createWindow);
